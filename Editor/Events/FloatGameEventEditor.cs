1d1d3c11e7c7a0b87bc4a217bbcc262e848fd73a
krzysztof biernat <krzysztof.biernat@edu.uekat.pl>
2020-07-15 19:18:03 +0200

    Add editor for float game event

---
+++ Editor/Events/FloatGameEventEditor.cs
@@ -0,0 +1,16 @@
+﻿using UnityEditor;
+
+namespace GTVariable.Editor
+{
+    [UnityEditor.CustomEditor(typeof(FloatGameEvent))]
+    public class FloatGameEventEditor 
+        : ParameterizedGameEventEditor<FloatGameEventListener, FloatGameEvent, FloatEvent, float>
+    {
+        protected override void DrawParameter()
+        {
+            EditorGUILayout.BeginVertical(EditorStyles.helpBox);
+            parameter = EditorGUILayout.FloatField("Parameter",parameter);
+            EditorGUILayout.EndVertical();
+        }
+    }
+}
