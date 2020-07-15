3a3a7ef1c3aa3c0dad0e5919b7c4221de93abb5e
krzysztof biernat <krzysztof.biernat@edu.uekat.pl>
2020-07-15 18:04:25 +0200

    Add property drawer for string variable

---
+++ Editor/Varaible/StringVariablePropertyDrawer.cs
@@ -0,0 +1,13 @@
+﻿using UnityEditor;
+
+namespace GTVariable.Editor
+{
+    [CustomPropertyDrawer(typeof(StringVariable))]
+    public class StringVariablePropertyDrawer : VariablePropertyDrawer
+    {
+        protected override void Init()
+        {
+            inlineDrawer.propertyPosition = VariableInlineDrawer.PropertyPosition.Under;
+        }
+    }
+}
