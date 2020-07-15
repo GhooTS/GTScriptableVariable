6b74e1067eccfdf6e9399d9658781f5107d07756
krzysztof biernat <krzysztof.biernat@edu.uekat.pl>
2020-07-15 18:12:42 +0200

    Add interface for one parameter game events listeners

---
+++ Runtime/Events/IRaisable.cs
@@ -0,0 +1,8 @@
+﻿namespace GTVariable
+{
+    public interface IRaisable<EventType,ParameterType>
+        where EventType : UnityEngine.Events.UnityEvent<ParameterType>
+    {
+        void OnEventRised(ParameterType value);
+    }
+}
