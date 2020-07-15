9ff813d0f610c527a6d9bbfdc761d726515ebf24
krzysztof biernat <krzysztof.biernat@edu.uekat.pl>
2020-07-15 18:03:02 +0200

    Integrate varialbe inline drawer

--- Editor/Varaible/VariablePropertyDrawer.cs
+++ Editor/Varaible/VariablePropertyDrawer.cs
@@ -1,11 +1,4 @@
 ﻿
-// ----------------------------------------------------------------------------
-// Unite 2017 - Game Architecture with Scriptable Objects
-// 
-// Author: Ryan Hipple
-// Date:   10/04/17
-// ----------------------------------------------------------------------------
-
 
 using UnityEditor;
 using UnityEngine;
@@ -16,58 +9,45 @@ using UnityEngine;
 namespace GTVariable.Editor
 {
 
+
     public class VariablePropertyDrawer : PropertyDrawer
     {
-        SerializedProperty valueProp;
-        SerializedObject serializedObject;
+        protected VariableInlineDrawer inlineDrawer = new VariableInlineDrawer();
+        private bool initalize = false;
+        protected virtual void Init()
+        {
+            inlineDrawer.inlineWidth = 60f;
+        }
 
         public override float GetPropertyHeight(SerializedProperty property, GUIContent label)
         {
-            return EditorGUI.GetPropertyHeight(property);
+            return EditorGUI.GetPropertyHeight(property) + inlineDrawer.GetHeight();
         }
 
         public override void OnGUI(Rect position, SerializedProperty property, GUIContent label)
         {
-            var propertyHeight = EditorGUI.GetPropertyHeight(property);
-            
+
+            if (initalize == false)
+            {
+                Init();
+                initalize = true;
+            }
+
+            var hasObjectReference = property.objectReferenceValue != null;
+            Rect inlinePosition = Rect.zero;
+
             EditorGUI.BeginChangeCheck();
+
+            if (hasObjectReference) inlinePosition = inlineDrawer.Reserve(ref position);
+
             position = EditorGUI.PrefixLabel(position,label);
-            var valuePosition = new Rect(position.xMax - 60, position.y,60, position.height);
-            position.width -= 65;
             EditorGUI.PropertyField(position, property,GUIContent.none);
             if (EditorGUI.EndChangeCheck())
             {
-                if (property.objectReferenceValue != null)
-                {
-                    serializedObject = new SerializedObject(property.objectReferenceValue);
-                }
+                inlineDrawer.Update(property);
             }
 
-            if (property.objectReferenceValue != null)
-            {
-                if (serializedObject == null)
-                {
-                    serializedObject = new SerializedObject(property.objectReferenceValue);
-                }
-                valueProp = serializedObject.FindProperty("value");
-            }
-            else
-            {
-                serializedObject = null;
-                valueProp = null;
-                return;
-            }
-            
-
-            serializedObject.Update();
-            
-            EditorGUI.BeginChangeCheck();
-            EditorGUI.PropertyField(valuePosition, valueProp,GUIContent.none);
-            if (EditorGUI.EndChangeCheck())
-            {
-                serializedObject.ApplyModifiedProperties();
-            }
-            
+            if(hasObjectReference) inlineDrawer.DrawProperty(property, inlinePosition);
         }
     }
 }
