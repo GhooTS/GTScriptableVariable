560e6f842e0b126869510be34ffdaf0a079b01eb
krzysztof biernat <krzysztof.biernat@edu.uekat.pl>
2020-07-15 18:20:40 +0200

    Add generic class for one parameter events and listeners

---
+++ Runtime/Events/ParameterizedGameEvent.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+namespace GTVariable
+{
+    public abstract class ParameterizedGameEvent<ListenerType, EventType, ParameterType> : ScriptableObject
+        where EventType : UnityEngine.Events.UnityEvent<ParameterType>
+        where ListenerType : IRaisable<EventType, ParameterType>
+    {
+        public List<ListenerType> EventListners { get { return eventListners; } }
+        protected readonly List<ListenerType> eventListners = new List<ListenerType>();
+
+        public void Raise(ParameterType value)
+        {
+            for (int i = eventListners.Count - 1; i >= 0; i--)
+            {
+                eventListners[i].OnEventRised(value);
+            }
+        }
+
+        public void RegisterListener(ListenerType listner)
+        {
+            eventListners.Add(listner);
+        }
+
+        public void UnRegisterListener(ListenerType listner)
+        {
+            eventListners.Remove(listner);
+        }
+    }
+}
---
+++ Runtime/Events/ParameterizedGameEventListener.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+
+namespace GTVariable
+{
+    public abstract class ParameterizedGameEventListener<GameEventType,EventType,ParameterType> : Listener,IRaisable<EventType,ParameterType>
+        where EventType : UnityEngine.Events.UnityEvent<ParameterType>
+        where GameEventType : ParameterizedGameEvent<IRaisable<EventType,ParameterType>,EventType,ParameterType>
+    {
+        public GameEventType[] gameEvents;
+        public EventType Response;
+
+        public void OnEventRised(ParameterType value)
+        {
+            Response?.Invoke(value);
+        }
+
+        private void OnEnable()
+        {
+            foreach (var gameEvent in gameEvents)
+            {
+                gameEvent.RegisterListener(this);
+            }
+
+        }
+
+        private void OnDisable()
+        {
+            foreach (var gameEvent in gameEvents)
+            {
+                gameEvent.UnRegisterListener(this);
+            }
+        }
+    }
+}
