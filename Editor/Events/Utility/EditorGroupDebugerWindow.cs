041dfb2fa3c9fe93b588594f1dac53e6a2ff8ba4
krzysztof biernat <krzysztof.biernat@edu.uekat.pl>
2020-07-24 17:14:07 +0200

    Add simple Editor Group debugger

---
+++ Editor/Events/Utility/EditorGroupDebugerWindow.cs
@@ -0,0 +1,99 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+using GTVariable.Editor.Utility;
+
+namespace GTVariable.Editor.Debugging
+{
+    public class EditorGroupDebugerWindow : EditorWindow
+    {
+        private MonoBehaviour[] components;
+        private MonoBehaviour[] prevComponents;
+        private Vector2 scrollVector;
+
+        [MenuItem("Window/ScriptableVars/Debug/EditorGrupoDebugger")]
+        public static void Init()
+        {
+            GetWindow<EditorGroupDebugerWindow>("Editor Group Debugger");
+        }
+
+        private void OnEnable()
+        {
+            position = WindowPrefs.LoadPosition(titleContent.text);
+            Selection.selectionChanged += UpdateComponentsList;
+            
+        }
+
+        private void OnDisable()
+        {
+            WindowPrefs.SavePosition(titleContent.text, position);
+            Selection.selectionChanged -= UpdateComponentsList;
+        }
+
+
+        private void OnInspectorUpdate()
+        {
+            if (Selection.activeGameObject != null)
+            {
+                components = Selection.activeGameObject.GetComponents<MonoBehaviour>();
+                Repaint();
+            }
+            else
+            {
+                components = null;
+            }
+        }
+
+
+
+        private void OnGUI()
+        {
+            if(Selection.activeGameObject != null && components != null)
+            {
+                scrollVector = EditorGUILayout.BeginScrollView(scrollVector);
+                EditorGUILayout.LabelField("Current Selected");
+                foreach (var component in components)
+                {
+                    if (component == null) continue;
+                    EditorGUILayout.BeginHorizontal();
+                    EditorGUILayout.LabelField(component.GetType().Name);
+                    EditorGUILayout.LabelField(component.hideFlags.ToString());
+                    EditorGUILayout.EndHorizontal();
+                }
+
+                if (prevComponents != null)
+                {
+                    EditorGUILayout.LabelField("Preview Selected");
+                    foreach (var component in prevComponents)
+                    {
+                        if (component == null) continue;
+                        EditorGUILayout.BeginHorizontal();
+                        EditorGUILayout.LabelField(component.GetType().Name);
+                        EditorGUILayout.LabelField(component.hideFlags.ToString());
+                        EditorGUILayout.EndHorizontal();
+                    }
+                }
+                EditorGUILayout.EndScrollView();
+            }
+            else
+            {
+                EditorGUILayout.LabelField("Select gameobject");
+            }
+        }
+
+
+        private void UpdateComponentsList()
+        {
+            if (Selection.activeGameObject != null)
+            {
+                if (components != null) prevComponents = components;
+                components = Selection.activeGameObject.GetComponents<MonoBehaviour>();
+                Repaint();
+            }
+            else
+            {
+                components = null;
+            }
+        }
+    }
+}
